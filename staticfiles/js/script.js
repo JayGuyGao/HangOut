bd93fca0e8f345743f18a6a9bf5ccc3e9109f162
Xianda Gao <gaoxiandathss@gmail.com>
2017-12-17 15:45:06 -0500

    d

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -17,6 +17,9 @@ function getUser() {
   var refreshToken = localStorage.getItem('hangout_refreshtoken');
   var account = localStorage.getItem('hangout_account');
   console.log('Account:' + account);
+  if (account == null) {
+    location.href = 'https://s3.amazonaws.com/group6-activity-website/login_form.html';
+  }
   $('#show-username').html('<span class="fa fa-user"></span>' + account);
 }
 
@@ -26,6 +29,48 @@ function logout() {
   localStorage.removeItem('hangout_refreshtoken');
   localStorage.removeItem('hangout_account');
   location.href = 'https://s3.amazonaws.com/group6-activity-website/login_form.html';
+}
+
+function fillInfo() {
+  var info = {
+    token: localStorage.getItem('hangout_accesstoken'),
+  }
+  $.ajax({
+        url: 'https://w217imcezl.execute-api.us-east-1.amazonaws.com/test/profile',
+        type: 'put',
+        dataType: 'json',
+        contentType: "application/json",
+        headers: {'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('hangout_idtoken'),
+                },
+        data: JSON.stringify(info),
+        success: function (data) {
+            //alert(data.msg);
+            console.log(data.id);
+            console.log(data.phone);
+            console.log(data.age);
+            console.log(data.gender);
+            console.log(data.nickname);
+            if (data.phone) {
+              $("input[name='phone']").val(data.phone);
+            }
+            if (data.age) {
+              $("input[name='age']").val(data.age);
+            }
+            if (data.nickname) {
+              $("input[name='nickname']").val(data.nickname);
+            }
+            if (data.gender == 'male') {
+              $("input[id='male']").attr('checked', true);
+              $("input[id='female']").attr('checked', false);
+            }
+            else {
+              $("input[id='male']").attr('checked', false);
+              $("input[id='female']").attr('checked', true);
+            }
+            $("input[name='sex']").val(data.gender);
+        },
+    });
 }
 
 function getActivities() {
@@ -157,6 +202,89 @@ function composeNewActivity(elements) {
   return obj;
 }
 
+
+function getMyAttActs() {
+    var info = {
+        id: localStorage.getItem('hangout_id'),
+        username: localStorage.getItem('hangout_account'),
+        token: localStorage.getItem('hangout_accesstoken'),
+    }
+    $.ajax({
+        url: 'https://w217imcezl.execute-api.us-east-1.amazonaws.com/test/profile',
+        type: 'put',
+        dataType: 'json',
+        contentType: "application/json",
+        headers: {'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('hangout_idtoken'),
+                },
+        data: JSON.stringify(info),
+        success: function (data) {
+            console.log(data);
+            renderMyActs(data.activities);
+        },
+    })
+}
+
+function getMyCrtActs() {
+    var info = {
+        id: localStorage.getItem('hangout_id'),
+        username: localStorage.getItem('hangout_account'),
+        token: localStorage.getItem('hangout_accesstoken'),
+    }
+    $.ajax({
+        url: 'https://w217imcezl.execute-api.us-east-1.amazonaws.com/test/profile',
+        type: 'put',
+        dataType: 'json',
+        contentType: "application/json",
+        headers: {'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('hangout_idtoken'),
+                },
+        data: JSON.stringify(info),
+        success: function (data) {
+            console.log(data);
+            renderMyActs(data.activities);
+        },
+    })
+}
+
+function renderMyActs(acts) {
+    var frame = document.getElementById('activities-frame');
+    acts.forEach(function(act) {
+        frame.appendChild(renderMyAct(act));
+    });
+}
+
+function renderMyAct(act) {
+    var node = createNode('div', ['col-lg-10', 'my-att-act']);
+    var divNode = createNode('div', ['col-lg-10', 'list-group']);
+    node.appendChild(divNode);
+
+    var actLinkNode = createNode('a', ['list-group-item', 'active']);
+    actLinkNode.setAttribute('href', '/activity_detail/' + act.id);
+    var actNameNode = createNode('h4', 'list-group-item-heading');
+    actNameNode.innerHTML = act.name;
+    var actTimeNode = createNode('h5', 'list-group-item-heading');
+    actTimeNode.innerHTML = act.start_time;
+    actLinkNode.appendChild(actNameNode);
+    actLinkNode.appendChild(actTimeNode);
+    divNode.appendChild(actLinkNode);
+
+    var actDescriptionNode = createNode('div', ['list-group-item']);
+    actDescriptionNode.innerHTML = "<h4 class='list-group-item-heading'> <span class='glyphicon glyphicon-map-marker' aria-hidden:true></span>" + act.place + "</h4><p class='list-group-item-text'>" + act.explanation + "</p>"
+    divNode.appendChild(actDescriptionNode);
+
+    var buttonNode = createNode('div', []);
+    if (act.status == "already_in") {
+        buttonNode.innerHTML = '<button type="submit" class="btn btn-md pull-right disabled" style="position:relative;top:4px">Joined</button>';
+    }
+    else if (act.status == "expired") {
+        buttonNode.innerHTML = '<button type="submit" class="btn btn-md pull-right disabled" style="position:relative;top:4px">Expired</button>';
+    }
+    else if (act.status == "available") {
+        buttonNode.innerHTML = '<form role="form" method="post" action=""><input type="hidden" name="activity_id" value={{activity.id}} /><input type="hidden" name="form_type" value="apply_activity" /><button type="submit" class="btn btn-md btn-danger pull-right" style="position:relative;top:4px">Apply</button></form>';
+    }
+    divNode.appendChild(buttonNode);
+}
 
 
 
