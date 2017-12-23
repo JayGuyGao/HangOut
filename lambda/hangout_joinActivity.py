38337a040b8fb91795765bec8d43f71640a6e1d2
El Shaddai <chenzijian13@gmail.com>
2017-12-22 21:55:02 -0500

    new and join

--- lambda/hangout_joinActivity.py
+++ lambda/hangout_joinActivity.py
@@ -1,12 +1,68 @@
 import boto3
+import os
+import datetime
+from botocore.vendored import requests
 
+DEFAULT_USER_POOL_ID = os.environ.get('DEFAULT_USER_POOL_ID')
+endpoint = "https://search-group6-activity-website-gv3gkyysjd5b7hnkji7hcmghzi.us-east-1.es.amazonaws.com/activities_test/activity/"
 db = boto3.resource("dynamodb")
 act_table = db.Table("Hangout-activity")
 attd_table = db.Table("Hangout-userattendance")
+idp_client = boto3.client('cognito-idp')
 
 def lambda_handler(event, context):
     userId = event["userId"]
     act_id = event["activity"]["act_id"]
+    r = requests.get(endpoint + act_id)
+    try:
+       act = r.json()["_source"]
+    except:
+       return {'Error': 'Cannot join the activity.'}
+    try:
+        min_age = act["min_age"]
+    except:
+        min_age = 0
+    try:
+        max_age = act["max_age"]
+    except:
+        max_age = 10000
+    try:
+        gen = act["sex_requirement"]
+    except:
+        gen = "unlimit"
+    try:
+        max_size = act["max_size"]
+    except:
+        max_size = 1000
+    try:
+        deadline = act["applyend_date"]
+    except:
+        deadline = "9999-99-99T99:99"
+    r = idp_client.admin_get_user(UserPoolId=DEFAULT_USER_POOL_ID,Username=userId)
+    item = {}
+    for attr in r['UserAttributes']:
+        if attr['Name'] == 'gender':
+            item['gender'] = attr['Value']
+        elif attr['Name'] == 'custom:age':
+            item['age'] = attr['Value']
+    if int(item['age']) > int(max_age) or int(item['age']) < int(min_age):
+       return {'Error': 'Not satisfy age requirement.'}
+    if item['gender'] != 'female' and ("woman" in gen):
+       return {'Error': 'Not satisfy gender requirement.'}
+    if item['gender'] != 'male' and ("only for man" in gen):
+       return {'Error': 'Not satisfy gender requirement.'}
+    try:
+        user_list = act_table.get_item(Key={"id": act_id})['Item']['userlist']
+    except:
+        user_list = []
+    if len(user_list) == max_size:
+        return {'Error': 'The quota is full.'}
+    if userId in user_list:
+       return {'Error': 'You have already joined the activity.'}
+    print(deadline)
+    print(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M"))
+    if datetime.datetime.now().strftime("%Y-%m-%dT%H:%M") > deadline:
+        return {'Error': 'The application deadlien has passed.'}
     try:
         act_table.update_item(Key={"id": act_id},
             UpdateExpression="set #userlist=list_append(if_not_exists(#userlist, :empty_list), :user)",
@@ -21,4 +77,3 @@ def lambda_handler(event, context):
     except:
         return {'Error': 'Cannot join activity.'}
     return {'Success': 'Congratulations.'}
-
--- lambda/hangout_newActivity.py
+++ lambda/hangout_newActivity.py
@@ -1,16 +1,55 @@
 from botocore.vendored import requests
 import boto3
+import base64
+import uuid
 
 db = boto3.resource("dynamodb")
 act_table = db.Table("Hangout-activity")
+attd_table = db.Table("Hangout-userattendance")
+s3 = boto3.resource("s3")
+bucket = s3.Bucket("hangout-new")
 
 endpoint = "https://search-group6-activity-website-gv3gkyysjd5b7hnkji7hcmghzi.us-east-1.es.amazonaws.com/activities_test/activity/"
 
 def lambda_handler(event, context):
     userid = event["userId"]
     event["activity"]["userId"] = userid;
+    if not event["activity"].has_key("picture"):
+        return {'Error': 'Please upload a picture.'}
+    if not event["activity"].has_key("place"):
+        return {'Error': 'Please set a location.'}
+    if not event["activity"].has_key("name"):
+        return {'Error': 'Please set a name.'}
+    if not event["activity"].has_key("start_time"):
+        return {'Error': 'Please set a start datetime.'}
+    if not event["activity"].has_key("applyend_time"):
+        return {'Error': 'Please set a application deadline.'}
+    if not event["activity"].has_key("end_time"):
+        return {'Error': 'Please set an end datetime.'}
+    startime = event["activity"]["start_time"]
+    endtime = event["activity"]["end_time"]
+    deadline = event["activity"]["applyend_time"]
+    print(deadline)
+    print(startime)
+    print(endtime)
+    if startime > endtime:
+        return {'Error': 'Start time cannot be later than end time.'}
+    if deadline > startime:
+        return {'Error': 'Application deadline cannot be later than start time.'}
+    (type, data) = event["activity"]["picture"].split(';', 1)
+    type = type.split('/')[1]
+    data = data.split(',')[1]
+    pic = base64.b64decode(data)
+    filename = "img/upload/{}.".format(uuid.uuid4()) + type
+    bucket.put_object(Key=filename, Body=pic, ACL='public-read')
+    event["activity"]["picture"] = filename
     r = requests.post(endpoint, json=event["activity"])
     res = r.json()
     act_id = res['_id']
     act_table.put_item(Item={"id": act_id, "userlist": [userid]})
+    attd_table.update_item(Key={"userid": userid},
+            UpdateExpression="set #actlist=list_append(if_not_exists(#actlist, :empty_list), :act)",
+            ExpressionAttributeNames={'#actlist': "activitylist"},
+            ExpressionAttributeValues={':act': [act_id], ':empty_list': []},
+            ReturnValues="UPDATED_NEW")
     return res
